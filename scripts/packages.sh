43fed099a690c8de341a7ac91c925c01ffbee779
tudaole <122336705+tudaole@users.noreply.github.com>
2025-03-15 23:03:13 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -40,7 +40,8 @@ git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
 #istore
 #git clone -b main --depth 1 https://github.com/linkease/istore.git
 #Packages
-git clone -b main --depth 1 https://github.com/217heidai/OpenWrt-Packages.git
+#git clone -b main --depth 1 https://github.com/217heidai/OpenWrt-Packages.git
+git clone -b main --depth 1 https://github.com/kiddin9/kwrt-packages
 
 popd
 
